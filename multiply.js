dd55fbfba2df292f937a3a22c91d1ddcd8bd702d
Thomas Vu <thomasjvu@gmail.com>
2022-04-05 18:29:01 -0400

    Add Codewars solution

---
+++ multiply.js
@@ -0,0 +1,12 @@
+// (8 kyu) Multiply
+/* Instructions
+ * This code does not execute properly. Try to figure out why.
+ */
+
+// Solution
+function multiply(a,b) {
+    return a * b
+}
+
+// Explanation
+// The code was missing the 'return' keyword before the expression. 
