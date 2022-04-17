fca6f2bcf9c3dc05f5f8217beca1efc583100381
Thomas Vu <thomasjvu@gmail.com>
2022-04-17 14:09:34 -0400

    Add Codewars solution

---
+++ forUFCFansTotalBeginners.js
@@ -0,0 +1,29 @@
+// (8 kyu)
+/* Instructions
+ * This is a beginner friendly kata especially for UFC/MMA fans.
+
+It's a fight between the two legends: Conor McGregor vs George Saint Pierre in Madison Square Garden. Only one fighter will remain standing, and after the fight in an interview with Joe Rogan the winner will make his legendary statement. It's your job to return the right statement depending on the winner!
+
+If the winner is George Saint Pierre he will obviously say:
+
+"I am not impressed by your performance."
+If the winner is Conor McGregor he will most undoubtedly say:
+
+"I'd like to take this chance to apologize.. To absolutely NOBODY!"
+Good Luck!
+
+Note
+The given name may varies in casing, eg., it can be "George Saint Pierre" or "geOrGe saiNT pieRRE". Your solution should treat both as the same thing (case-insensitive).
+ */
+
+// Solution
+const quote = fighter => {
+  if (fighter.toUpperCase() === 'GEORGE SAINT PIERRE') {
+    return "I am not impressed by your performance."
+  } else if (fighter.toUpperCase() === "CONOR MCGREGOR") {
+    return "I'd like to take this chance to apologize.. To absolutely NOBODY!"
+  }
+};
+
+// Explanation
+// Use the string `.toUpperCase()` method to check if the string is equal to its uppercase equivalent.
