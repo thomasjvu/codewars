2fe3e6939b3b31b4277f3d7a801c1187a52d6d7e
thomasjvu <thomasjvu@gmail.com>
2022-05-15 17:47:43 -0400

    Add Codewars solution

---
+++ 7kyu/deodorantEvaporator.js
@@ -0,0 +1,33 @@
+// (7 kyu) Deodorant Evaporator
+/* Instructions
+ * This program tests the life of an evaporator containing a gas.
+
+We know the content of the evaporator (content in ml), the percentage of foam or gas lost every day (evap_per_day) and the threshold (threshold) in percentage beyond which the evaporator is no longer useful. All numbers are strictly positive.
+
+The program reports the nth day (as an integer) on which the evaporator will be out of use.
+Example:
+
+evaporator(10, 10, 5) -> 29
+
+Note:
+
+Content is in fact not necessary in the body of the function "evaporator", you can use it or not use it, as you wish. Some people might prefer to reason with content, some other with percentages only. It's up to you but you must keep it as a parameter because the tests have it as an argument.
+*/
+
+// Solution
+function evaporator(content, evap_per_day, threshhold) {
+    let days = 0
+    let percentage = 100
+    while (percentage > threshhold) {
+        percentage = percentage - percentage * (evap_per_day / 100)
+        days += 1
+    }
+    return days
+}
+
+// Explanation
+// Create a new variable to store the # of days
+// Set new percentage variable to 100%
+// Create a while loop that checks if the current percent is greater than the threshhold
+// if it is, add 1 day to the days variable, until the percentage loop is less than the threshhold
+// return the days variable
