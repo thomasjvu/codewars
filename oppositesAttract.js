20b7119a71e37f3951141dff33f5e4f16fc617e3
Thomas Vu <thomasjvu@gmail.com>
2022-06-13 10:56:07 -0400

    Add Codewars solution

---
+++ oppositesAttract.js
@@ -0,0 +1,39 @@
+// (8 kyu) Opposites Attract
+/* Instructions
+ * Timmy & Sarah think they are in love, but around where they live, they will only know once they pick a flower each. If one of the flowers has an even number of petals and the other has an odd number of petals it means they are in love.
+
+Write a function that will take the number of petals of each flower and return true if they are in love and false if they aren't.
+*/
+
+// Solution
+function lovefunc(flower1, flower2) {
+    if (flower1 % 2 === 0 && flower2 % 2 !== 0) {
+        return true
+    } else if (flower1 % 2 !== 0 & flower2 % 2 === 0) {
+        return true
+    } else {
+        return false 
+    }
+}
+
+// Refactored
+const lovefunc = (flower1, flower2) => {
+    if ((flower1 + flower2) % 2 !== 0) {
+        return true
+    } else {
+        return false
+    }
+}
+
+// Refactored (Again)
+// Refactored
+const lovefunc = (flower1, flower2) => {
+    if ((flower1 + flower2) % 2 !== 0) {
+        return true
+    } else {
+        return false
+    }
+}
+
+// Explanation
+// If flower1 + flower2 is odd, that means that an even + odd number was added together, so we can get the sum and check if the modulus is not equal to 0.
