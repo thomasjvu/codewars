e2d4ea9ce9dbb77453d0a2875294aa1d9915944f
Thomas Vu <thomasjvu@gmail.com>
2022-06-14 13:38:18 -0400

    Add Codewars solution

---
+++ volumeOfACuboid.js
@@ -0,0 +1,14 @@
+// (8 kyu) Volume of a Cuboid
+/* Instructions
+Bob needs a fast way to calculate the volume of a cuboid with three values: the length, width and height of the cuboid. Write a function to help Bob with this calculation.
+*/
+
+// Solution
+class Kata {
+  static getVolumeOfCuboid(length, width, height) {
+    return length * width * height
+  }
+}
+
+// Explanation
+// Use the formula: volume = length * width * height to get the volume of a cuboid
