752d5eb833b2aec6fcf3a0fa17ce37054fb2714f
Thomas Vu <thomasjvu@gmail.com>
2022-07-05 12:35:02 -0400

    Convert input string to uppercase.

---
+++ makeUpperCase.js
@@ -0,0 +1,13 @@
+// (8 Kyu) MakeUpperCase
+
+// Description
+
+/*
+Write a function which converts the input string to uppercase.
+*/
+
+
+// Solution 1
+function makeUpperCase(str) {
+    return str.toUpperCase()
+  }
