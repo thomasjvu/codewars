b222b96c1a14947361ba0062596ea1a87fa0d432
Thomas Vu <thomasjvu@gmail.com>
2022-09-06 10:43:25 -0400

    Add Codewars (SQL) solution

---
+++ sql/7kyu/helloSQLWorld.sql
@@ -0,0 +1,16 @@
+/* (7 kyu) Hello SQL World!
+ * Instructions
+     Hello SQL!
+
+    Return a table with a single column named Greeting with the phrase 'hello world!'
+
+    Please use Data Manipulation Language and not Data Definition Language to solve this Kata
+
+*/
+
+/* Solution */
+SELECT 'hello world!' as "Greeting"
+
+/* Explanation 
+ * For the column content, wrap it in SINGLE QUOTES
+ * For the column name, wrap it in DOUBLE QUOTES
