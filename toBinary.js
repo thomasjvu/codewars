85208ec968886d6b4313b52b88f6f010c8a2c9fa
Thomas Vu <thomasjvu@gmail.com>
2022-10-19 17:25:44 -0400

    Add refactored code

--- toBinary.js
+++ toBinary.js
@@ -8,6 +8,9 @@ function toBinary(n){
     return Number(n.toString(2));
 }
 
+// Refactored (Arrow Function)
+const toBinary = n => Number(n.toString(2))
+
 // Explanation
 // The `.toString()` method can take a radix as the parameter. The radix 2 is for binary numbers, so using that
 // in our argument enables us to convert the number to a binary string. Then, we convert the binary string back to
