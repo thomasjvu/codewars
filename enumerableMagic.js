e3f3c5af0484eb7049b735656c532c422a4ad2be
Thomas Vu <thomasjvu@gmail.com>
2022-10-21 12:02:27 -0400

    Add refactoring

--- enumerableMagic.js
+++ enumerableMagic.js
@@ -10,3 +10,8 @@ function include(arr, item){
 // Explanation
 // We can use the array method `.includes()` to check if our array includes a certain value, which in this case is item.
 // The ternary operator is used to say if the condition is true, return true, otherwise return false.
+
+// Refactored
+const include = (arr, item) => arr.includes(item)
+
+// This can be reduced into an arrow function that doesn't need the ternary operator because the `.includes()` method will inherently return a true or false value.
