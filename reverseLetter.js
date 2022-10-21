ebadb27bf2e244acdccf96edbe1ae903c91f67e7
Thomas Vu <thomasjvu@gmail.com>
2022-10-21 01:52:27 -0400

    Add Codewars solution + refactoring

---
+++ reverseLetter.js
@@ -0,0 +1,28 @@
+// (7 kyu) Simple Fun #176: Reverse Letter 
+// Task
+
+// Given a string str, reverse it and omit all non-alphabetic characters.
+// Example
+
+// For str = "krishan", the output should be "nahsirk".
+
+// For str = "ultr53o?n", the output should be "nortlu".
+// Input/Output
+
+//     [input] string str
+
+// A string consists of lowercase latin letters, digits and symbols.
+
+//     [output] a string
+
+
+
+/* Solution */
+
+// function reverseLetter(str) {
+//   return str.split('').reverse().join('').replace(/[^a-z]/ig, '') // carat symbol in brackets means "not ____"
+// }
+
+// Refactored
+const reverseLetter = str => str.split('').reverse().join('').replace(/[^a-z]/ig, '')
+
