2524aa71d96e9b7ae89430a778c68b0d55e0cd0c
Thomas Vu <thomasjvu@gmail.com>
2022-12-03 12:31:19 -0500

    Add Codewars solution

---
+++ defineACardSuit.js
@@ -0,0 +1,35 @@
+// (8 kyu) Define a card suit
+/* Instructions
+ * You get any card as an argument. Your task is to return the suit of this card (in lowercase).
+
+Our deck (is preloaded):
+
+('3♣') -> return 'clubs'
+('3♦') -> return 'diamonds'
+('3♥') -> return 'hearts'
+('3♠') -> return 'spades'
+
+*/
+
+// Solution
+function defineSuit(card) {
+    switch (card[1]) {
+        case '♣':
+            return 'clubs'
+            break
+        case '♦':
+            return 'diamonds'
+            break
+        case '♥':
+            return 'hearts'
+            break
+        case '♠':
+            return 'spades'
+            break
+    }
+}
+
+// Explanation
+// Use a switch..case statement with the element at index 1 for the card string to be switched
+// let each card suit be a case for the switched string element
+// return the corresponding string for each case
