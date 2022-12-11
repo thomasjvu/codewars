3d7b28b9842ea05f9428f7ceae048b53335d810a
Thomas Vu <thomasjvu@gmail.com>
2022-12-11 17:34:30 -0500

    Add C# Codewars solution

---
+++ c#/8kyu/multiply.cs
@@ -0,0 +1,14 @@
+// (8 kyu) Multiply
+/* Instructions
+ * This code does not execute properly. Try to figure out why
+ */
+
+// Solution
+public class CustomMath {
+    public static int multiply(int a, intb) {
+        return a * b
+    }
+}
+
+// Explanation
+// The code was missing the asterisk between integer a and integer b
