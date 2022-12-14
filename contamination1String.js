d2a71dde0503f1fdfa3dcf3f5a65340ff7f9876f
Thomas Vu <thomasjvu@gmail.com>
2022-12-14 12:18:03 -0500

    Add Codewars solution

---
+++ contamination1String.js
@@ -0,0 +1,33 @@
+// (8 kyu) Contamination #1 -String-
+/* Instructions
+ * An AI has infected a text with a character!!
+
+This text is now fully mutated to this character.
+
+If the text or the character are empty, return an empty string.
+There will never be a case when both are empty as nothing is going on!!
+
+Note: The character is a string of length 1 or an empty string.
+Example
+
+text before = "abc"
+character   = "z"
+text after  = "zzz"
+*/
+
+// Solution
+function contamination(text, char){
+    let str = ""
+    for (let i = 0; i < text.length; i++) {
+        str += char
+    }
+
+    return str
+}
+
+
+// Explanation
+// Declare a new empty string
+// Use a for loop through iterate through each element within the given text.
+// For each iteration, concatenate the given char to the existing string
+// Return the new filled out string
