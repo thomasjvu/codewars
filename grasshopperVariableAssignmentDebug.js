c188ce182ea95f02ed2478c4fe8e35d897d6f500
Thomas Vu <thomasjvu@gmail.com>
2022-12-18 20:46:44 -0500

    Add Codewars solution

---
+++ grasshopperVariableAssignmentDebug.js
@@ -0,0 +1,16 @@
+// (8 kyu) Grasshopper - Variable Assignment Debug
+/* Instructions
+ * Variable assignment
+
+Fix the variables assigments so that this code stores the string 'devLab' in the variable name.
+*/
+
+// Solution
+var a = "dev"
+var b = "Lab"
+
+var name = a + b
+
+// Explanation
+// The undebugged solution consisted of 2 equal signs, which is not correct for assignment.
+// Assignment only uses one equal sign, so that was the fix needed for each variable.
