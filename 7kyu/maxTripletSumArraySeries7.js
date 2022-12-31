116e5cbe1f669d827c64ad9813276b41cd592992
Thomas Vu <thomasjvu@gmail.com>
2022-12-30 19:31:26 -0500

    Add Codewars solution

---
+++ 7kyu/maxTripletSumArraySeries7.js
@@ -0,0 +1,53 @@
+// (7 kyu) maximum Triplet Sum (Array Series #7)
+/* Instructions
+ * Task
+
+Given an array/list [] of n integers , find maximum triplet sum in the array Without duplications .
+Notes :
+
+    Array/list size is at least 3 .
+
+    Array/list numbers could be a mixture of positives , negatives and zeros .
+
+    Repetition of numbers in the array/list could occur , So (duplications are not included when summing).
+    Input >> Output Examples
+
+1- maxTriSum ({3,2,6,8,2,3}) ==> return (17)
+
+Explanation:
+
+    As the triplet that maximize the sum {6,8,3} in order , their sum is (17)
+
+    Note : duplications are not included when summing , (i.e) the numbers added only once .
+
+    2- maxTriSum ({2,1,8,0,6,4,8,6,2,4}) ==> return (18)
+
+    Explanation:
+
+    As the triplet that maximize the sum {8, 6, 4} in order , their sum is (18) ,
+
+    Note : duplications are not included when summing , (i.e) the numbers added only once .
+
+    3- maxTriSum ({-7,12,-7,29,-5,0,-7,0,0,29}) ==> return (41)
+
+    Explanation:
+
+    As the triplet that maximize the sum {12 , 29 , 0} in order , their sum is (41) ,
+
+    Note : duplications are not included when summing , (i.e) the numbers added only once .
+*/
+
+// Solution
+function maxTriSum(numbers) {
+    let unique = new Set(numbers)
+    let arr = Array.from(unique)
+
+    arr.sort((a, b) => b - a)
+    return arr[0] + arr[1] + arr[2]
+}
+
+// Explanation
+// Get a set of unique numbers from the given array using new Set()
+// Turn that into an array using Array.from()
+// Sort the array in descending order using .sort((a,b => b - a)
+// Return the first 3 elements in the array added together
