0360cfe29c59c10a00e01b472778ac577e3d139e
Thomas Vu <thomasjvu@gmail.com>
2023-02-01 13:22:07 -0500

    Add Codewars solution

---
+++ classicHelloWorld.js
@@ -0,0 +1,27 @@
+// (8 kyu) Classic Hello World
+/* Instructions
+You are given a method called main, make it print the line Hello World!, (yes, that includes a new line character at the end) and don't return anything
+
+Note that for some languages, the function main is the entry point of the program.
+
+Here's how it will be tested:
+
+    Solution.main("parameter1","parameter2");
+
+Hints:
+
+    Check your references
+    Think about the scope of your method
+    For prolog you can use write but there are better ways
+    If you still don't get it probably you can define main as an attribute of the Solution class that accepts a single argument, and that only prints "Hello World!" without any return.
+*/
+
+// Solution
+class Solution{
+    static main() {
+        console.log('Hello World!')
+    }
+}
+
+// We want to print the line, and not return anything, so we will be using console.log('Hello World!')
+// Our function is named main()
