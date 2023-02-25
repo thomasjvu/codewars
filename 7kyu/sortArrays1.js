da935e63d839e0275173864ed991592c8d715ccc
Thomas Vu <thomasjvu@gmail.com>
2023-02-24 20:43:18 -0500

    Add Codewars solution

---
+++ 7kyu/sortArrays1.js
@@ -0,0 +1,14 @@
+// (7 kyu) Sort Arrays - 1
+/* Instructions
+Just a simple sorting usage. Create a function that returns the elements of the input-array / list sorted in lexicographical order.
+*/
+
+// Solution
+// sortme = function(names) {
+//     return names.sort()
+// }
+
+const sortme = names => names.sort()
+
+// Explanation
+// Just use the .sort() method, which without any arguments, sorts the items in alphabetical (lexicographical) order.
