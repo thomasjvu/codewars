713c5c1aba2649bcdd4588f05cac250a962ea88d
Thomas Vu <thomasjvu@gmail.com>
2023-05-04 13:21:38 -0400

    Add codewars solution

---
+++ grasshopperObjectSyntaxDebug.js
@@ -0,0 +1,46 @@
+// (8 kyu) Grasshopper - Object Syntax Debug
+/* Instructions
+Object debugging
+
+While making a zork-type game, you create an object of rooms. Unfortunately, the game is not working. Find all of the errors in the rooms object to get your game working again.
+*/
+
+// Original
+// var rooms = {
+//   first: {
+//     description: 'This is the first room'
+//     items: {
+//       chair: 'The old chair looks comfortable',
+//       lamp: 'This lamp looks ancient'
+//   },
+//   second: {
+//     description: 'This is the second room'
+//     items: {
+//       couch: 'This couch looks like it would hurt your back,
+//       table: 'On the table there is an unopened bottle of water'
+//     }
+//   }
+// }
+
+// Solution
+var rooms = {
+  first: {
+      description: 'This is the first room',
+      items: {
+        chair: 'The old chair looks comfortable',
+        lamp: 'This lamp looks ancient'
+    },
+    second: {
+      description: 'This is the second room',
+      items: {
+        couch: 'This couch looks like it would hurt your back',
+        table: 'On the table there is an unopened bottle of water'
+      }
+    }
+  }
+}
+
+// Explanation
+// Add commas after non-last object properties
+// Make sure each string is properly closed
+// Add forgotten closing bracket
