7020fcb0f56d07aed38885fe25f7beddbf3e02a9
Thomas Vu <thomasjvu@gmail.com>
2023-05-10 21:54:17 -0400

    Add Typescript solution

--- reversedStrings.js
+++ reversedStrings.js
@@ -6,11 +6,21 @@
     'word'   =>  'drow'
 */
 
-// Solution
-
+/* Solution (JavaScript) */
 function solution(str){
   return str.split('').reverse().join('')
 }
 
 // Explanation
 // To reverse a string, `.split()` it to an array, use the `.reverse()` method, and then `.join()` it back to a string.
+
+/* Solution (TypeScript) */
+// export function solution(str: string): string {
+//     return str.split('').reverse().join('')
+// }
+
+/* Solution (TypeScript => Arrow Function) */
+// export const solution = (str: string): string => str.split('').reverse().join('')
+
+// identify the parameter str as a string type
+// identify the function return to be a string type
